c47056827c7a62392f22000daae3dd5c81600688
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-08-13 20:03:40 -0300

    Ajusta responsividade de home

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,54 +3,34 @@ import GitHubLogo from '../assets/githubLogo.svg?react';
 import LinkedinLogo from '../assets/linkedinLogo.svg?react';
 import ArrowBotton from '../assets/ArrowBotton.svg?react';
 
+import style from './Main.module.css';
+
 const Main = () => {
   return (
     <>
-      <main className="flex justify-between items-center h-screen px-56 relative">
-        <section className="flex flex-col w-1/2">
-          <span className="text-cinza-500 dark:text-cinza-300 font-semibold text-2xl font-Poppins">
+      <main
+        className={`pt-16 w-screen h-screen flex flex-col gap-2 justify-center items-center p-3 lg:flex-row lg:px-20 xl:px-56`}
+      >
+        <div className="lg:order-2 lg:w-1/3 flex justify-end">
+          <div className="rounded-sm h-44 w-44 md:w-56 md:h-56 ">
+            <div className={style.image}></div>
+          </div>
+        </div>
+
+        <section className="text-cinza-50 flex flex-col justify-center items-center gap-2 lg:w-2/3 lg:items-start ">
+          <p className="font-Roboto font-bold text-cinza-300 text-lg md:text-2xl">
             Rhuan Guilherme
-          </span>
-          <span className="text-cinza-800 dark:text-cinza-100 font-bold text-5xl font-Poppins">
+          </p>
+          <h1 className="font-Poppins font-bold text-cinza-100 text-2xl md:text-4xl">
             Desenvolvedor Full Stack
-          </span>
-          <span className="text-cinza-500 dark:text-cinza-300 font-Roboto mt-1 text-base leading-7">
+          </h1>
+          <p className="font-Roboto  text-cinza-300 text-sm text-center leading-6 md:text-base lg:text-left">
             Seja bem vindo o meu portifolio, sou um desenvolvedor com
             experiência em tecnologias front-end e back-end. Minha especialidade
             é criar soluções eficientes usando Node.js e React. Explore meu
             portfólio para conhecer mais sobre meus projetos e habilidades.
-          </span>
-          <div className="flex gap-2 mt-1 font-Poppins font-medium text-cinza-800 dark:text-cinza-200">
-            <a
-              href="https://github.com/Rhuan-Guilherme"
-              target="_blank"
-              className="border-2 border-cinza-800 dark:border-cinza-300 rounded-[4px] py-1 px-4 flex items-center gap-3 text-lg hover:scale-105 transition-all duration-300"
-              rel="noreferrer"
-            >
-              <GitHubLogo />
-              Github
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rhuanguilherme23/"
-              target="_blank"
-              className="border-2 border-cinza-800 dark:border-cinza-300 rounded-[4px] py-1 px-4 flex items-center gap-3 text-lg hover:scale-105 transition-all duration-300"
-              rel="noreferrer"
-            >
-              <LinkedinLogo />
-              Linkedin
-            </a>
-          </div>
+          </p>
         </section>
-        <section className="w-1/2 flex justify-end">
-          <div className="border-2 border-cinza-800 dark:border-cinza-300 p-0.5 rounded-lg w-[500px] shadow-md">
-            <img className="w-full rounded-md" src="./rhuan.jpg" alt="" />
-          </div>
-        </section>
-
-        <div className="absolute bottom-6 left-1/2 text-cinza-300 font-medium font-Roboto flex flex-col items-center text-sm animate-bounce">
-          SCROLL
-          <ArrowBotton />
-        </div>
       </main>
     </>
   );
